617dc21493ff70fb2c2f800380898d7d8c8e6ca9
KozyrevAlexey <guper2000@mail.ru>
2022-12-25 22:20:32 +0300

    fix: Внести красоту в код

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,33 @@
-const popupElement = document.querySelector('.popup');
-const popupElementClose = popupElement.querySelector('.popup__button-close');
-const popupElementOpen = document.querySelector('.profile__edit-buton');
-const popupElementForm = popupElement.querySelector('.popup__form');
+const popupElement = document.querySelector('.popup'); //Найти popup
+const popupElementClose = popupElement.querySelector('.popup__button-close'); //Найти кнопку закрытия
+const popupElementOpen = document.querySelector('.profile__edit-buton'); //Найти кнопку открытия редактирования профиля
+const popupElementForm = popupElement.querySelector('.popup__form'); //Найти форму popup изменения профиля
 
-let profileName = document.querySelector('.profile-info__title');
-let profileJob = document.querySelector('.profile-info__intro');
-let inputName = document.querySelector('.popup__input_type_name');
-let inputJob = document.querySelector('.popup__input_type_job');
+let profileName = document.querySelector('.profile-info__title'); //Найти данные - name на странице
+let profileJob = document.querySelector('.profile-info__intro'); //Найти данные - job на странице
+let inputName = document.querySelector('.popup__input_type_name'); //Найти поле ввода - name в форме
+let inputJob = document.querySelector('.popup__input_type_job'); //Найти поле ввода - job в форме
 
+//Функция открытия popup с данными name и job указанных на странице
 const openPopup = function () {
   popupElement.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
 }
 
+//Функция закрытия popup без сохранения внесенных данных
 const closePopup = function () {
   popupElement.classList.remove('popup_opened');
 }
 
+//Функция закрытия при нажатии на overlay. Сейчас не используется
 // const closePopupClickOverlay = function (event) {
 //   if (event.target === event.currentTarget) {
 //     closePopup();
 //   }
 // }
 
+//Функция сохранения внесенных в формы popup изменений при закрытии окна
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
@@ -31,6 +35,7 @@ function handleFormSubmit(evt) {
   closePopup();
 }
 
+//Слушатели событий
 popupElementOpen.addEventListener('click', openPopup);
 popupElementClose.addEventListener('click', closePopup);
 // popupElement.addEventListener('click', closePopupClickOverlay);
