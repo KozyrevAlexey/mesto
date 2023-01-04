bdfbaa69764f1a013278611ee0ec5a6bbad96c72
KozyrevAlexey <guper2000@mail.ru>
2023-01-04 20:23:39 +0300

    feat: Подготовить разметку в Html

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
-const popupElement = document.querySelector('.popup'); //Найти popup
-const popupElementClose = popupElement.querySelector('.popup__button-close'); //Найти кнопку закрытия
+const popupProfile = document.querySelector('.popup__profile'); //Найти popup
+const popupElementClose = popupProfile.querySelector('.popup__button-close'); //Найти кнопку закрытия
 const popupElementOpen = document.querySelector('.profile__edit-buton'); //Найти кнопку открытия редактирования профиля
-const popupElementForm = popupElement.querySelector('.popup__form'); //Найти форму popup изменения профиля
+const popupElementForm = popupProfile.querySelector('.popup__form'); //Найти форму popup изменения профиля
 
 let profileName = document.querySelector('.profile-info__title'); //Найти данные - name на странице
 let profileJob = document.querySelector('.profile-info__intro'); //Найти данные - job на странице
@@ -10,14 +10,14 @@ let inputJob = document.querySelector('.popup__input_type_job'); //Найти п
 
 //Функция открытия popup с данными name и job указанных на странице
 const openPopup = function () {
-  popupElement.classList.add('popup_opened');
+  popupProfile.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
 }
 
 //Функция закрытия popup без сохранения внесенных данных
 const closePopup = function () {
-  popupElement.classList.remove('popup_opened');
+  popupProfile.classList.remove('popup_opened');
 }
 
 //Функция закрытия при нажатии на overlay. Сейчас не используется
@@ -40,3 +40,23 @@ popupElementOpen.addEventListener('click', openPopup);
 popupElementClose.addEventListener('click', closePopup);
 // popupElement.addEventListener('click', closePopupClickOverlay);
 popupElementForm.addEventListener('submit', handleFormSubmit);
+
+
+//Функция лайк-дизлайк карточки
+// const toggleLike = (element) => {
+//   element.addEventListener('click', function (evt) {
+//     evt.target.classList.toggle('element__button_active');
+//   });
+// }
+
+const toggleLike = document.querySelector('.element__button');
+
+// toggleLike.addEventListener('click', function (event) { // event доступен как параметр
+//   const eventTarget = evt.target;
+//     eventTarget.setAttribute('disabled', true);
+// });
+
+
+toggleLike.querySelector('.element__button').addEventListener('click', function (evt) {
+  evt.target.classList.toggle('.element__button_active'/* передайте название класса */);
+});
