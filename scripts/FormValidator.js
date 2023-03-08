1d09a543d5a4348ee45a998435e74c3594bfa190
Alexey Kozyrev <guper095@gmail.com>
2023-03-08 12:19:32 +0300

    fix: Внести исправления на замечания после проверки

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,5 +1,5 @@
 class FormValidator {
-  constructor(config, form){
+  constructor(config, form) {
     this._inputSelector = config.inputSelector;
     this._submitButtonSelector = config.submitButtonSelector;
     this._inactiveButtonClass = config.inactiveButtonClass;
@@ -7,15 +7,14 @@ class FormValidator {
     this._errorClass = config.errorClass;
     this._formSelector = config.formSelector;
     this._form = form;
-    this._form = document.querySelector(form);
   }
 
   /**Функция валидации формы */
-enableValidation() {
-  this._addInputListners();
-};
+  enableValidation() {
+    this._addInputListners();
+  };
 
- /**Добавить класс ошибки */
+  /**Добавить класс ошибки */
   _showInputError(inputElement) {
     const errorElement = this._form.querySelector(`#${inputElement.id}-error`);
     inputElement.classList.add(this._inputErrorClass);
@@ -35,10 +34,10 @@ enableValidation() {
   _handleFormInput(inputElement) {
     if (!inputElement.validity.valid) {
       this._showInputError(inputElement);
-  }  else {
-    this._hideInputError(inputElement);
+    } else {
+      this._hideInputError(inputElement);
+    }
   }
-}
 
   /**Функция переключения кнопки сабмит */
   _toggleButton() {
@@ -48,24 +47,25 @@ enableValidation() {
     this._buttonSubmint.classList.toggle(this._inactiveButtonClass, !this._isFormValid);
   }
 
- /**Объявить функцию слушателей всех инпутов */
-_addInputListners() {
-  this._toggleButton();
-  this._inputList = this._form.querySelectorAll(this._inputSelector);
-  this._inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', () => {
-      this._handleFormInput(inputElement);
-      this._toggleButton();
-    });
-  })
-};
-
-/**Сбросить валидацию после закрытия формы */
-clearValidationForm() {
-  this._toggleButton();
-  this._inputList.forEach((inputElement) => {
-    this._hideInputError(inputElement);
+  /**Объявить функцию слушателей всех инпутов */
+  _addInputListners() {
+    this._toggleButton();
+    this._inputList = this._form.querySelectorAll(this._inputSelector);
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._handleFormInput(inputElement);
+        this._toggleButton();
+      });
     })
+  };
+
+  /**Сбросить валидацию после закрытия формы */
+  clearValidationForm() {
+    this._toggleButton();
+    this._inputList.forEach((inputElement) => {
+      this._hideInputError(inputElement);
+    })
+  }
 }
-}
+
 export { FormValidator };
